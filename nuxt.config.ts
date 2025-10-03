49a6304dc4a7779412861194bb703b703e7270b6
deixadilson <deixadilson@deixadilson.com>
2025-10-03 11:56:16 -0300

    feat: initial project setup with nuxt and supabase

---
+++ nuxt.config.ts
@@ -0,0 +1,11 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/supabase'],
+  supabase: {
+    url: process.env.SUPABASE_URL,
+    key: process.env.SUPABASE_KEY,
+    redirect: false
+  }
+});
